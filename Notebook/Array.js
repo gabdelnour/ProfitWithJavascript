3b95261c14f60f462cb1deb51d901a1a4fe58000
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-07-13 18:50:19 -0400

    Array Operations

---
+++ Notebook/Array.js
@@ -0,0 +1,12 @@
+let styles = ["Jazz", "Blues"];
+console.log(styles);
+styles.push("Rock-n-Roll");
+console.log(styles);
+
+styles[Math.round((styles.length - 1) / 2)] = "Classic";
+console.log(styles);
+
+let firstItem = styles.shift();
+
+styles.unshift("Rap", "Reggae");
+console.log(styles);
