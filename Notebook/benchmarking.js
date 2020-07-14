e395f3b7123eaca99bf39560bbf38ba03075e3c6
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-07-14 12:24:33 -0400

    how to measure time complexity of a function in seconds

---
+++ Notebook/benchmarking.js
@@ -0,0 +1,28 @@
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
+
+function bench(f) {
+  let date1 = new Date(0);
+  let date2 = new Date();
+
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) f(date1, date2);
+  return Date.now() - start;
+}
+
+let time1 = 0;
+let time2 = 0;
+
+// run bench(upperSlice) and bench(upperLoop) each 10 times alternating
+for (let i = 0; i < 10; i++) {
+  time1 += bench(diffSubtract);
+  time2 += bench(diffGetTime);
+}
+
+console.log("Total time for diffSubtract: " + time1);
+console.log("Total time for diffGetTime: " + time2);
