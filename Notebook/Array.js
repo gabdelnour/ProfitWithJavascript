3cbce63d6928775f4716b7872800d2a0f79972a7
Gustavo Abdelnour <gurabdelnour@gmail.com>
2020-07-13 21:50:11 -0400

    guess the output exercise

--- Notebook/Array.js
+++ Notebook/Array.js
@@ -1,12 +1,24 @@
-let styles = ["Jazz", "Blues"];
-console.log(styles);
-styles.push("Rock-n-Roll");
-console.log(styles);
+// let styles = ["Jazz", "Blues"];
+// console.log(styles);
+// styles.push("Rock-n-Roll");
+// console.log(styles);
 
-styles[Math.round((styles.length - 1) / 2)] = "Classic";
-console.log(styles);
+// styles[Math.round((styles.length - 1) / 2)] = "Classic";
+// console.log(styles);
 
-let firstItem = styles.shift();
+// let firstItem = styles.shift();
 
-styles.unshift("Rap", "Reggae");
-console.log(styles);
+// styles.unshift("Rap", "Reggae");
+// console.log(styles);
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// let arr = ["a", "b"];
+
+// arr.push(function () {
+//   console.log(this);
+// });
+
+// arr[2]();
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
